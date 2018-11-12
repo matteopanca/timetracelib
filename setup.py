4e0dc45c6be3f4de686d5e881cebf3d4784dfd9a
Matteo Pancaldi <matteo.pancaldi89@gmail.com>
2018-11-12 19:32:20 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='timetracelib',
+      version='1.0',
+	  author='Matteo Pancaldi',
+      author_email='matteo.pancaldi@fysik.su.se',
+      url='https://github.com/matteopanca/timetracelib',
+      packages=['timetrace'],
+     )
---
+++ timetrace/__init__.py
@@ -0,0 +1,2 @@
+#this can be an empty file
+#https://docs.python.org/3/tutorial/modules.html#packages
