f85035d87d071ca0a61d8b350c12a46ba7619bf0
Matteo Pancaldi <matteo.pancaldi89@gmail.com>
2018-11-12 20:04:39 +0100

    Separated HystLoop from SingleTrace

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='timetracelib',
       version='1.0',
-	  author='Matteo Pancaldi',
+      author='Matteo Pancaldi',
       author_email='matteo.pancaldi@fysik.su.se',
       url='https://github.com/matteopanca/timetracelib',
       packages=['timetrace'],
--- timetrace/__init__.py
+++ timetrace/__init__.py
@@ -1,2 +1,16 @@
 #this can be an empty file
 #https://docs.python.org/3/tutorial/modules.html#packages
+
+import tkinter as tk
+from tkinter import filedialog
+
+#----- Useful parameters -----
+figsize_double = (18, 8)
+figsize_single = (12, 8)
+#-----------------------------
+
+#Get the selected file's path in a string
+def get_path(start_path='', filter=(('Data file', '*.dat'),('All files', '*'))):
+	root = tk.Tk() #hide the root window
+	root.withdraw() #hide the root window
+	return filedialog.askopenfilename(initialdir=start_path, title='Select input file', filetypes=filter)
