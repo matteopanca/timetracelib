252acf130aaf82bb0c812926cbff4fe64b3c8990
Matteo Pancaldi <matteo.pancaldi89@gmail.com>
2019-02-01 19:39:12 +0100

    Added fmr_kittel

--- timetrace/__init__.py
+++ timetrace/__init__.py
@@ -4,6 +4,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from PyQt5.QtWidgets import QApplication, QFileDialog
+import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
@@ -11,6 +12,20 @@ import sys
 figsize_double = (18, 8)
 figsize_single = (12, 8)
 #-----------------------------
+
+def fmr_kittel(h, m, n, unit='mT'):
+	#m is the saturation magnetization in A/m
+	#demag. factors, keeping in mind that n[2] is the applied field direction
+	mu0 = np.pi*4.0e-7
+	gamma = (2.0023*1.6021766e-19/(2*9.109383e-31))/(np.pi*2.0e9) #GHz/T
+	if unit == 'Oe':
+		h_conv = np.complex_(1e-4*h/mu0) #from Oe to A/m
+	elif unit == 'mT':
+		h_conv = np.complex_(1e-3*h/mu0) #from mT to A/m
+	else:
+		raise RuntimeError('Unit not yet supported')
+	kittel_freq = mu0*gamma*np.real(np.sqrt((h_conv + (n[0] - n[2])*m)*(h_conv + (n[1] - n[2])*m)))
+	return kittel_freq
 
 def load_image(file_name, img_size=(5, 5)):
 	image = plt.imread(file_name)
